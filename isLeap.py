bfe16b59e8fa0babc137e428f751a37a15c76a6f
Flavio Andrade <flavioandrade@Flavios-MBP.home>
2016-12-18 20:57:35 -0500

    Created isLeap
    Python program to determine if a given year is a leap year.

--- isLeap.py
+++ isLeap.py
@@ -1,7 +1,7 @@
 def is_leap(year):
 	"""
 	For a year to be a leap year three things criterias need to be looked at.
-	1. The year is evenly divisible by four
+	1. The year is evenly divisible by four.
 	2. If the year can be divided by 100 it is not a leap year, unless it can
 	also be divided by 400.
 	"""
